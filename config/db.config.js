e794ff918a59ffa54449619a4d71120f7267a721
Adittiya Asril <adittiyaasril369@gmail.com>
2024-04-26 20:48:27 +0700

    ready to deploy

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
+
 module.exports = {
-  HOST: "127.0.0.1",
-  USER: "postgres",
-  PASSWORD: "230798",
-  DB: "inventory-db",
-  dialect: "postgres",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   pool: {
     max: 5,
     min: 0,
