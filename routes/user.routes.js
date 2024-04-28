f79ce08f6fd15ce012b53dab1fb9416924a0656f
Adittiya Asril <adittiyaasril369@gmail.com>
2024-04-28 18:09:49 +0700

    feat: add route for logout

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,7 @@ const UserController = require("../controllers/user.controller");
 
 router.post("/", UserController.createUser);
 router.post("/login", UserController.loginUser);
+router.post("/logout", UserController.logoutUser);
 router.get("/", UserController.getUsers);
 router.get("/:id", UserController.getUserById);
 router.put("/:id", UserController.updateUser);
