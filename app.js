b07f31d7aed6a15fdce2e53d2ffcb37cf33458ed
Adittiya Asril <adittiyaasril369@gmail.com>
2024-06-16 18:26:33 +0700

    fix: change cors domain

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://soal3-fe.vercel.app"],
+    origin: ["http://localhost:3000", "https://store-inv.vercel.app"],
     credentials: true,
   })
 );
